dadeb2535e8c92226adac5929e74560d9979279c
Jepryanuslewi <jeprylk3773@gmail.com>
2024-05-19 21:09:29 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,47 @@
 #include "header.h"
-#include <iostream>
 
 int main() {
-    Mahasiswa* daftar = nullptr;
-    int pilihan;
-    string nama;
-    int pertemuan;
-
+    simpul L = NULL;
+    int choose;
     do {
-        cout << "\n========================\n";
-        cout << "          Menu";
-        cout << "\n========================\n";
-        cout << "|1. Tambah Mahasiswa\n";
-        cout << "|2. Tambah Kehadiran\n";
-        cout << "|3. Cetak Semua Mahasiswa\n";
-        cout << "|4. Keluar";
-        cout << "\n========================\n";
-        cout << "Pilih opsi: ";
-        cin >> pilihan;
-
-        switch (pilihan) {
-            case 1:
-                cout << "Masukkan nama mahasiswa: ";
-                cin.ignore();
-                getline(cin, nama);
-                daftar = tambahMahasiswa(daftar, nama);
+        cout<< "--------------------------\n"
+            << "           MENU\n"
+            << "--------------------------\n"
+            << " 1. Tambah Mahasiswa\n"
+            << " 2. Tambah Kehadiran Mahasiswa\n"
+            << " 3. Cetak Seluruh Data\n"
+            << " 0. Keluar\n"
+            << "--------------------------\n"
+            << "Masukan pilihan : ";
+        cin >> choose;
+        switch (choose) {
+        case 1:
+            {
+                string nama;
+                cout << "Masukkan Nama Mahasiswa: ";
+                cin >> nama;
+                Sisip(L, nama);
+                cout << "Mahasiswa " << nama << " ditambahkan." << endl;
                 break;
-            case 2:
-                cout << "Masukkan nama mahasiswa: ";
-                cin.ignore();
-                getline(cin, nama);
-                cout << "Masukkan nomor pertemuan (1-14): ";
+            }
+        case 2:
+            {
+                string nama;
+                int pertemuan;
+                cout << "Masukkan Nama Mahasiswa: ";
+                cin >> nama;
+                cout << "Masukkan Nomor Pertemuan: ";
                 cin >> pertemuan;
-                tambahKehadiranMahasiswa(daftar, nama, pertemuan);
+                TambahKehadiran(L, nama, pertemuan);
                 break;
-            case 3:
-                cetakSemuaMahasiswa(daftar);
-                break;
-            case 4:
-                cout << "Keluar dari program.\n";
-                break;
-            default:
-                cout << "Pilihan tidak valid, coba lagi.\n";
-                break;
+            }
+        case 3:
+            Cetak(L);
+            break;
+        default:
+            cout << "Pilihan tidak tersedia";
+            break;
         }
-    } while (pilihan != 4);
-
-    hapusSemuaMahasiswa(daftar);
-
+    } while (choose != 0);
     return 0;
 }
